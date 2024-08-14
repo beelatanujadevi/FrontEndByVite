eddaa04235cf1b95d706af186f8ad83d51f4ab53
keshavsoft <nknnkumar@yahoo.com>
2024-08-14 16:28:37 +0530

    started ledgers in accounts

--- src/Clients/Accounts/Sst/SourceCode/Js/pages/ReceiptsWithGroups/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
+++ src/Clients/Accounts/Sst/SourceCode/Js/pages/ReceiptsWithGroups/FormLoad/AddListeners/RefreshBSTableId/ButtonClickFunc.js
@@ -3,9 +3,7 @@ import { StartFunc as Openings } from "./Openings/Entry.js";
 
 import { StartFunc as StartFuncAfterFetch } from "./AfterFetch/EntryFile.js";
 
-
 let StartFunc = async () => {
-
     let [a, b] = await Promise.all([Accounts(), Openings()]);
 
     StartFuncAfterFetch({ inAccounts: a, inOpenings: b });
