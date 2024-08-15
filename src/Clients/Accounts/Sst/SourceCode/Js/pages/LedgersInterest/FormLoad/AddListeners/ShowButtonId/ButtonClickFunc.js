3035e451e811e5a3384317b7102f1b1b22c5a454
keshavsoft <nknnkumar@yahoo.com>
2024-08-15 17:00:30 +0530

    ledgers interest working good in sst

--- src/Clients/Accounts/Sst/SourceCode/Js/pages/LedgersInterest/FormLoad/AddListeners/ShowButtonId/ButtonClickFunc.js
+++ src/Clients/Accounts/Sst/SourceCode/Js/pages/LedgersInterest/FormLoad/AddListeners/ShowButtonId/ButtonClickFunc.js
@@ -13,7 +13,55 @@ let StartFunc = async () => {
 
     const arrByID = jVarGlobalPresentViewData.filter(filterByAccountName);
 
-    StartFuncAfterFetch({ inData: arrByID });
+    const jVarLocalInterestPer = jFLocalInterestInputId();
+    const jVarLocalToDate = jFLocalDateInputId();
+
+    const jVarLocalWithInterest = jFLocalAddInterest({
+        inData: arrByID,
+        inInterestPer: parseInt(jVarLocalInterestPer),
+        inToDate: jVarLocalToDate
+    });
+
+    StartFuncAfterFetch({ inData: jVarLocalWithInterest });
+};
+
+let jFLocalInterestInputId = () => {
+    let jVarLocalInterestInputId = 'InterestInputId'
+    let jVarLocalHtmlId = document.getElementById(jVarLocalInterestInputId);
+
+    if (jVarLocalHtmlId === null === false) {
+        return jVarLocalHtmlId.value.trim();
+    };
+};
+
+const jFLocalAddInterest = ({ inData, inInterestPer, inToDate }) => {
+    let jVarLocalInterestAdded = inData.map(element => {
+        let jVarLocalDays = jFLocalGetDays({
+            inFromDate: element.Date,
+            inToDate
+        });
+
+        if (element.Credit > 0) {
+            element.CreditInterest = ((element.Credit * inInterestPer * jVarLocalDays) / (365 * 100)).toFixed(2);
+        };
+
+        if (element.Debit > 0) {
+            element.DebitInterest = ((element.Debit * inInterestPer * jVarLocalDays) / (365 * 100)).toFixed(2);
+        };
+
+        return element;
+    });
+
+    return jVarLocalInterestAdded;
+};
+
+let jFLocalGetDays = ({ inFromDate, inToDate }) => {
+    var date1 = new Date(inFromDate)//converts string to date object
+    var date2 = new Date(inToDate)
+
+    var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+    var diffDays = Math.abs((date1.getTime() - date2.getTime()) / (oneDay));
+    return diffDays;
 };
 
 function filterByAccountName(item) {
@@ -22,6 +70,15 @@ function filterByAccountName(item) {
     };
 
     return false;
-}
+};
+
+let jFLocalDateInputId = () => {
+    let jVarLocalDateInputId = 'DateInputId'
+    let jVarLocalHtmlId = document.getElementById(jVarLocalDateInputId);
+
+    if (jVarLocalHtmlId === null === false) {
+        return jVarLocalHtmlId.value.trim();
+    };
+};
 
 export { StartFunc }
--- src/Clients/Accounts/Sst/SourceCode/Js/pages/LedgersInterest/FormLoad/StartFunc.js
+++ src/Clients/Accounts/Sst/SourceCode/Js/pages/LedgersInterest/FormLoad/StartFunc.js
@@ -6,6 +6,17 @@ const StartFunc = () => {
     AccountsFill();
     StartFuncBuildBsTable();
     StartFuncAddListeners();
+
+    jFLocalToInputDateInputId(new Date().toJSON().slice(0, 10));
+};
+
+let jFLocalToInputDateInputId = (inValue) => {
+    let jVarLocalHtmlId = 'DateInputId';
+    let jVarLocalDateInputId = document.getElementById(jVarLocalHtmlId);
+
+    if (jVarLocalDateInputId === null === false) {
+        jVarLocalDateInputId.value = inValue;
+    };
 };
 
 export { StartFunc };
