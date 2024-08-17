9276d4f2aa9cdb915a8c32356aa85dccc58731e2
keshavsoft <nknnkumar@yahoo.com>
2024-08-17 12:03:45 +0530

    daybook single working

--- src/Clients/Accounts/Sst/SourceCode/Js/pages/DayBooksSingle/FormLoad/AddListeners/ShowButtonId/ButtonClickFunc.js
+++ src/Clients/Accounts/Sst/SourceCode/Js/pages/DayBooksSingle/FormLoad/AddListeners/ShowButtonId/ButtonClickFunc.js
@@ -4,7 +4,8 @@ import { StartFunc as Payments } from "./Payments/Entry.js";
 import { StartFunc as Journals } from "./Journals/Entry.js";
 
 import { StartFunc as StartFuncAfterFetch } from "./AfterFetch/EntryFile.js";
-const CommonAccountNameSelected = document.getElementById("AccountsInputId");
+const CommonFromDateInputId = document.getElementById("FromDateInputId");
+const CommonToDateInputId = document.getElementById("ToDateInputId");
 
 let StartFunc = async () => {
     let [a, b, c, d] = await Promise.all([Receipts(), Openings(), Payments(), Journals()]);
@@ -13,72 +14,15 @@ let StartFunc = async () => {
 
     const arrByID = jVarGlobalPresentViewData.filter(filterByAccountName);
 
-    const jVarLocalInterestPer = jFLocalInterestInputId();
-    const jVarLocalToDate = jFLocalDateInputId();
-
-    const jVarLocalWithInterest = jFLocalAddInterest({
-        inData: arrByID,
-        inInterestPer: parseInt(jVarLocalInterestPer),
-        inToDate: jVarLocalToDate
-    });
-
-    StartFuncAfterFetch({ inData: jVarLocalWithInterest });
-};
-
-let jFLocalInterestInputId = () => {
-    let jVarLocalInterestInputId = 'InterestInputId'
-    let jVarLocalHtmlId = document.getElementById(jVarLocalInterestInputId);
-
-    if (jVarLocalHtmlId === null === false) {
-        return jVarLocalHtmlId.value.trim();
-    };
-};
-
-const jFLocalAddInterest = ({ inData, inInterestPer, inToDate }) => {
-    let jVarLocalInterestAdded = inData.map(element => {
-        let jVarLocalDays = jFLocalGetDays({
-            inFromDate: element.Date,
-            inToDate
-        });
-
-        if (element.Credit > 0) {
-            element.CreditInterest = ((element.Credit * inInterestPer * jVarLocalDays) / (365 * 100)).toFixed(2);
-        };
-
-        if (element.Debit > 0) {
-            element.DebitInterest = ((element.Debit * inInterestPer * jVarLocalDays) / (365 * 100)).toFixed(2);
-        };
-
-        return element;
-    });
-
-    return jVarLocalInterestAdded;
-};
-
-let jFLocalGetDays = ({ inFromDate, inToDate }) => {
-    var date1 = new Date(inFromDate)//converts string to date object
-    var date2 = new Date(inToDate)
-
-    var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-    var diffDays = Math.abs((date1.getTime() - date2.getTime()) / (oneDay));
-    return diffDays;
+    StartFuncAfterFetch({ inData: arrByID });
 };
 
 function filterByAccountName(item) {
-    if (item.AccountName === CommonAccountNameSelected.value) {
+    if (item.Date >= CommonFromDateInputId.value && item.Date <= CommonToDateInputId.value) {
         return true;
     };
 
     return false;
-};
-
-let jFLocalDateInputId = () => {
-    let jVarLocalDateInputId = 'DateInputId'
-    let jVarLocalHtmlId = document.getElementById(jVarLocalDateInputId);
-
-    if (jVarLocalHtmlId === null === false) {
-        return jVarLocalHtmlId.value.trim();
-    };
-};
+}
 
 export { StartFunc }
