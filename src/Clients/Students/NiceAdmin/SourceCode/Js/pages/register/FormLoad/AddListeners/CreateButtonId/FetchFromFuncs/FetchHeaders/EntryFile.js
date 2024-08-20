12d43f5b39f0bf2861a82255e40904f8afd20c8c
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2024-08-20 21:56:48 +0530

    student register mail send working

--- src/Clients/Students/NiceAdmin/SourceCode/Js/pages/register/FormLoad/AddListeners/CreateButtonId/FetchFromFuncs/FetchHeaders/EntryFile.js
+++ src/Clients/Students/NiceAdmin/SourceCode/Js/pages/register/FormLoad/AddListeners/CreateButtonId/FetchFromFuncs/FetchHeaders/EntryFile.js
@@ -1,26 +1,26 @@
 import KeysJson from './Keys.json' with {type: 'json'};
 
 let StartFunc = () => {
-    KeysJson.body.StudentName = jFLocalStudentName();
-    KeysJson.body.StudentName = jFLocalMobile();
+    KeysJson.body.StudentName = jFLocalStudentNameId();
+    KeysJson.body.Mobile = jFLocalMobileId();
     KeysJson.body.Email = jFLocalEmailId();
     KeysJson.body = JSON.stringify(KeysJson.body);
 
     return KeysJson;
 };
 
-let jFLocalStudentName = () => {
-    let jVarLocalStudentName = 'StudentName'
-    let jVarLocalHtmlId = document.getElementById(jVarLocalStudentName);
+let jFLocalStudentNameId = () => {
+    let jVarLocalStudentNameId = 'StudentNameId'
+    let jVarLocalHtmlId = document.getElementById(jVarLocalStudentNameId);
 
     if (jVarLocalHtmlId === null === false) {
         return jVarLocalHtmlId.value.trim();
     };
 };
 
-let jFLocalMobile = () => {
-    let jVarLocalMobile = 'Mobile'
-    let jVarLocalHtmlId = document.getElementById(jVarLocalMobile);
+let jFLocalMobileId = () => {
+    let jVarLocalMobileId = 'MobileId'
+    let jVarLocalHtmlId = document.getElementById(jVarLocalMobileId);
 
     if (jVarLocalHtmlId === null === false) {
         return jVarLocalHtmlId.value.trim();
