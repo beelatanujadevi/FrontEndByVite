78414d894b4b87b5185c9c4139e42ccf154959d6
sharath0036 <ksharathbabu2@gmail.com>
2024-08-26 12:58:30 +0530

    changing urljson to config json

--- src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/MTableShowSTAdd/FormLoad/BuildBsTable/onClickRow/EntryFile.js
+++ src/AllTables/WithSSR/SubTableSideBar/SourceCode/JsTemplate/pages/MTableShowSTAdd/FormLoad/BuildBsTable/onClickRow/EntryFile.js
@@ -1,11 +1,11 @@
-import UrlJson from './Url.json' with {type: "json"};
+import ConfigJson from '../../../Config.json' with {type: "json"};
 const StartFunc = (row, $element, field) => {
     console.log("current field: ", field);
     if (field === "STAdd") {
 
         const url = new URL(window.location.href);
         const params1 = new URLSearchParams(url.search);
-        let NewURl = new URL(`./${jVarGlobalTableName}${UrlJson.RedirectUrl}`, url);
+        let NewURl = new URL(`./${jVarGlobalTableName}${ConfigJson.RedirectUrl}`, url);
         const new_url = new URL(`${NewURl.href}?${params1}`);
         new_url.searchParams.append('inRowPk', row.pk);
 
