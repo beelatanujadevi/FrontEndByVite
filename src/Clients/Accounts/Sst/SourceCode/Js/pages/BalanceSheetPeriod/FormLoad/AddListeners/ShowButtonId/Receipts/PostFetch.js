88908f268b4f0ac47b1c56c0afa5bf3a310c961e
sharath0036 <ksharathbabu2@gmail.com>
2024-08-26 14:02:37 +0530

    making changes in balance sheet

--- src/Clients/Accounts/Sst/SourceCode/Js/pages/BalanceSheetPeriod/FormLoad/AddListeners/ShowButtonId/Receipts/PostFetch.js
+++ src/Clients/Accounts/Sst/SourceCode/Js/pages/BalanceSheetPeriod/FormLoad/AddListeners/ShowButtonId/Receipts/PostFetch.js
@@ -1,5 +1,9 @@
+import ConfigJson from '../../../../Config.json' with {type: "json"};
+
 let StartFunc = async () => {
-    let jVarLocalFetchUrl = `/bin/Receipts/DataOnly`;
+    let jVarLocalRoutePath = ConfigJson.routePath; 
+
+    let jVarLocalFetchUrl = `/${jVarLocalRoutePath}/Receipts/DataOnly`;
     let response = await fetch(jVarLocalFetchUrl);
 
     return await response;
