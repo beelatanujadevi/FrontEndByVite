5bd4822ed0c63b75ebfef66bc49a9f0f71e8cc2c
sharath0036 <ksharathbabu2@gmail.com>
2024-08-27 11:16:35 +0530

    changing config in accounts for reports

--- src/Clients/Accounts/Sst/SourceCode/Js/pages/AccountsWithGroups/FormLoad/AddListeners/RefreshBSTableId/Accounts/PostFetch.js
+++ src/Clients/Accounts/Sst/SourceCode/Js/pages/AccountsWithGroups/FormLoad/AddListeners/RefreshBSTableId/Accounts/PostFetch.js
@@ -1,9 +1,10 @@
-// import ConfigJson from '../../../../Config.json' with {type: 'json'};
+import ConfigJson from '../../../../Config.json' with {type: 'json'};
 
 let StartFunc = async () => {
     let jVarLocalTableName = jVarGlobalTableName;
+    let jVarLocalRoutePath = ConfigJson.routePath; 
 
-    let jVarLocalFetchUrl = `/bin/Accounts/DataOnly`;
+    let jVarLocalFetchUrl = `/${jVarLocalRoutePath}/Accounts/DataOnly`;
     let response = await fetch(jVarLocalFetchUrl);
 
     return await response;
