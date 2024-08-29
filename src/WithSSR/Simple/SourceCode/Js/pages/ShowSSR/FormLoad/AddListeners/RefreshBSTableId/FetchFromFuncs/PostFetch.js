c5cafc8455f4bef7838db3bc97810f18e09abe85
sharath0036 <ksharathbabu2@gmail.com>
2024-08-29 17:56:55 +0530

    changing bin to binV3

--- src/WithSSR/Simple/SourceCode/Js/pages/ShowSSR/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/PostFetch.js
+++ src/WithSSR/Simple/SourceCode/Js/pages/ShowSSR/FormLoad/AddListeners/RefreshBSTableId/FetchFromFuncs/PostFetch.js
@@ -1,9 +1,9 @@
-// import ConfigJson from '../../../../Config.json' with {type: 'json'};
+ import ConfigJson from '../../../../Config.json' with {type: 'json'};
 
 let StartFunc = async () => {
     let jVarLocalTableName = jVarGlobalTableName;
 
-    let jVarLocalFetchUrl = `/bin/${jVarLocalTableName}/DataOnly`;
+    let jVarLocalFetchUrl = `/${ConfigJson.routePath}/${jVarLocalTableName}/DataOnly`;
     let response = await fetch(jVarLocalFetchUrl);
 
     return await response;
